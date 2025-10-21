6d53b60cb49912d6ab8fd16b3a473a00537c22e6
Leonardo A. Pregnolato <leopregnolato@outlook.com>
2025-10-21 19:34:19 -0300

    Commit inicial

---
+++ src/main/java/com/dws/application/LeopregnolatoApplication.java
@@ -0,0 +1,13 @@
+package com.dws.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeopregnolatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeopregnolatoApplication.class, args);
+	}
+
+}
